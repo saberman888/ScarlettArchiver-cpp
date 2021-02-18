5cd7527c428e738e09e88453f264e490b875f9ac
saberman888 <sagittariusz548@gmail.com>
2021-02-18 16:42:42 -0700

    Split RedditCommon into two separate classes: Linkable and Postable

--- Scarlett/Reddit/RedditCommon.cpp
+++ Scarlett/Reddit/RedditCommon.cpp
@@ -2,55 +2,59 @@
 
 namespace ScarlettArchiver::RedditAsset
 {
-	bool RedditCommon::operator==(const RedditCommon& other)
+	bool Postable::operator==(Postable& other)
 	{
-		return (URL == other.URL && Id == other.Id && Author == other.Author && Permalink == other.Permalink && Title == other.Title && Domain == other.Domain);
+		return (timediff(mktime(&CreatedUTC), mktime(&other.CreatedUTC)) && (Author == other.Author) && (Id == other.Id) && (Permalink == other.Permalink));
 	}
-	bool RedditCommon::operator!=(const RedditCommon& other)
+
+	bool Postable::operator!=(Postable& other)
 	{
-		return !this->operator==(other);
+		return (!timediff(mktime(&CreatedUTC), mktime(&other.CreatedUTC)) && (Author != other.Author) && (Id != other.Id) && (Permalink != other.Permalink));
 	}
-	void RedditCommon::initLog()
-	{
-		log = spdlog::get("Scarlett Archiver Log");
-	}
-	void RedditCommon::Read(const nlohmann::json& json)
+
+	void Postable::Read(const nlohmann::json& json)
 	{
 		try {
-
-			Id = json.at("id").get<std::string>();
-			log->info("ID: " + Id);
-
-
-			Author = json.at("author").get<std::string>();
-
-			Permalink = json.at("permalink").get<std::string>();
-			log->info("Permalink: " + Permalink);
-
-			if(json.contains("title"))
-				Title = json.at("title").get<std::string>(); 
-
-			CreatedUTC = json.at("created_utc").get<time_t>();
-
-			if (json.contains("link_url"))
-			{
-				URL = json.at("link_url").get<std::string>();
-			}
-			else if(json.contains("url")){
-				URL = json.at("url").get<std::string>();
-			}
-			
-			if (json.contains("domain") && json.at("domain").is_string())
-				Domain = json.at("domain").get<std::string>();
+			json.at("author").get_to(Author);
+			json.at("id").get_to(Id);
+			json.at("permalink").get_to(Permalink);
+			time_t elemTime = json.at("created_utc").get<time_t>();
+			CreatedUTC = *std::gmtime(&elemTime);
 		}
-		catch (nlohmann::json::exception& e) {
-			scarlettNestedThrow("Failed to parse JSON for RedditCommon, " + std::string(e.what()));
-
+		catch (const nlohmann::json::exception& e) {
+			scarlettNestedThrow(e.what());
 		}
 	}
-
-	void RedditCommon::SerializeTo(SerializationMethod sm)
+	bool Postable::timediff(unsigned int end, unsigned int begin)
 	{
-		SerializeAs = sm;
+		return std::difftime(
+			max(end, begin),
+			min(end, begin)
+		) == 0;
+	}
+	bool Linkable::operator==(const Linkable& other)
+	{
+		return ((Domain == other.Domain) && (Title == other.Title) && (URL == other.URL));
+	}
+	bool Linkable::operator!=(const Linkable& other)
+	{
+		return ((Domain != other.Domain) && (Title != other.Title) && (URL != other.URL));
+	}
+	void Linkable::Read(const nlohmann::json& json)
+	{
+		try {
+			json.at("domain").get_to(Domain);
+			json.at("title").get_to(Title);
+			if (json.contains("url"))
+			{
+				json.at("url").get_to(URL);
+			}
+			else {
+				json.at("link_url").get_to(URL);
+			}
+		}
+		catch (const nlohmann::json::exception& e) {
+			scarlettNestedThrow(e.what());
+		}
 	}
 }
--- Scarlett/Reddit/RedditCommon.hpp
+++ Scarlett/Reddit/RedditCommon.hpp
@@ -2,107 +2,78 @@
 
 #include <string>
 #include <ctime>
-#include <filesystem>
-#include <optional>
+#include <algorithm>
 #include "nlohmann/json.hpp"
-#include <fstream>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
-#include <omp.h>
 /*#include <boost/archive/xml_iarchive.hpp>
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/serialization/nvp.hpp>*/
 #include "exceptions.hpp"
 #include "Logger.hpp"
-#include "Comment.hpp"
-
 
 
 namespace ScarlettArchiver::RedditAsset
 {
-
-	enum class SerializationMethod
-	{
-		Text,
-		Binary,
-		XML
-	};
-
-	/**
-		RedditCommon is an ancestor class for all postable Reddit components to inherit from. 
-		It has common values that are shared between Links, Self Posts, Images, Videos, Galleries and Comments
+	/*
+	* Postable is an class that represents common values that Videos, Images, Galleries, Self Posts and Comments have
 	*/
-	class RedditCommon
+	class Postable
 	{
 	public:
-		/**
-		* A constructor with an empty signature. It only initializes SerializeAs and increments NumberOfInstances
-		*/
-		RedditCommon() : URL(std::nullopt), Title(std::nullopt), Domain(std::nullopt), CreatedUTC(0), SerializeAs(SerializationMethod::Text) {}
-		void SerializeTo(SerializationMethod sm = SerializationMethod::Text);
-
-		/**
-			Common values that are shared between: Links, Self Posts, Images, Videos, Galleries and Comments
-			URL, Title and Domain are optional values because, these tags are absent from Comments
-		*/
-		std::string Id, Author, Permalink;
-		std::optional<std::string> URL, Title, Domain;
-
-		// Time the post was created in Unix Epoch Time
+		Postable(){}
 		time_t CreatedUTC;
-
-		virtual bool operator==(const RedditCommon& other);
-		virtual bool operator!=(const RedditCommon& other);
-
-		/**
-		* Serializes the class using Boost's serialization library and writes it to a file
-		* @param A directory to put the serializes class and a filename
-		*/
-		//void Write(std::filesystem::path destination, std::string filename);
-		std::shared_ptr<spdlog::logger> log;
-		void initLog();
-	protected:
-		/**
-		* Parses and assigns values from Reddit's json into the class' values
-		*/
-		virtual void Read(const nlohmann::json& json);
+		std::string Author;
+		std::string Id;
+		std::string Permalink;
+		virtual bool operator==(Postable& other);
+		virtual bool operator!=(Postable& other);
+		inline struct tm GetUTCTime()
+		{
+			return *std::gmtime(&CreatedUTC);
+		}
+	private:
 		friend class boost::serialization::access;
 		template<class Archive>
 		void serialize(Archive& ar, const unsigned int version)
 		{
-			if (SerializeAs == SerializationMethod::Text)
-			{
-				TextSerialize(ar, version);
-			}
-			else if (SerializeAs == SerializationMethod::XML) {
-				//XmlSerialize(ar, version);
-			}
-		}
-
-		SerializationMethod SerializeAs;
-		template<class Archive>
-		void TextSerialize(Archive& ar, const unsigned int version)
-		{
-			ar& URL.value();
-			ar& Id;
-			ar& Author;
-			ar& Permalink;
-			ar& Title.value();
-			ar& Domain.value();
 			ar& CreatedUTC;
+			ar& Author;
+			ar& Id;
+			ar& Permalink;
 		}
 
-		/*template<class Archive>
-		void XmlSerialize(Archive& ar, const unsigned int version)
-		{
-			ar& boost::serialization::make_nvp("URL", URL);
-			ar& boost::serialization::make_nvp("Id", Id);
-			ar& boost::serialization::make_nvp("Author", Author);
-			ar& boost::serialization::make_nvp("Permalink", Permalink);
-			ar& boost::serialization::make_nvp("Title", Title);
-			ar& boost::serialization::make_nvp("Domain", Domain);
-			ar& boost::serialization::make_nvp("CreatedUTC", CreatedUTC);
-		}*/
+	protected:
+		void Read(const nlohmann::json& json);
+		bool timediff(unsigned int end, unsigned int begin);
+	};
 
+	/*
+	* Linkable is an class that represents common values that Videos, Images, Galleries and Self Posts have
+	*/
+	class Linkable
+	{
+	public:
+		/*
+		* Empty constructor for boost serialization
+		*/
+		Linkable() {}
+		std::string Domain;
+		std::string Title;
+		std::string URL;
+		virtual bool operator==(const Linkable& other);
+		virtual bool operator!=(const Linkable& other);
+	private:
+		friend class boost::serialization::access;
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& Domain;
+			ar& Title;
+			ar& URL;
+		}
+
+	protected:
+		void Read(const nlohmann::json& json);
 	};
 };
