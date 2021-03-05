b2b2541922f4a92c43faa80d9c20f45737c670e9
saberman888 <sagittariusz548@gmail.com>
2021-03-05 03:50:01 -0700

    Changed some parameters from nlohmann json to JSON::value and included a boost serialization function

--- Scarlett/Reddit/Comment.hpp
+++ Scarlett/Reddit/Comment.hpp
@@ -16,14 +16,26 @@ namespace ScarlettArchiver::RedditAsset
 	{
 	public:
 		Comment(const std::string& ParentId);
-		Comment(const nlohmann::json& json, std::optional<std::string> ParentId = std::nullopt);
+		Comment(const JSON::value& json, std::optional<std::string> ParentId = std::nullopt);
 		std::vector<std::unique_ptr<Comment>> replies; 
 		void GetRedditComments();
 		void GetPushShiftComments();
 
+		friend class boost::serialization::access;
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& boost::serialization::base_object<TextPost>(*this);
+			if (ParentId)
+				ar& ParentId.value();
+			else
+				ar& std::string();
+		}
+
+
 	private:
 		std::optional<std::string> ParentId;
-		void Read(const nlohmann::json& json);
+		void Read(const JSON::value& json);
 	};	
 	using CommentTree = std::vector<std::unique_ptr<Comment>>;
 };
