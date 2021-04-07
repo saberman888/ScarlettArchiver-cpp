79187c38ab0379e0f615b5c6c58aa6c39f205509
saberman888 <sagittariusz548@gmail.com>
2021-04-06 23:37:14 -0600

    Remove PushShift namespace and wrap all thefunctions in class with the same name and a RateTracker variable

--- Vun/PushShift.cpp
+++ Vun/PushShift.cpp
@@ -1,25 +1,25 @@
 #include "PushShift.hpp"
 #include <fstream>
 #include <iostream>
-namespace Vun::PushShift {
+namespace Vun {
 
-	static Response PushShiftGet(std::string endpoint, const std::map<std::string, std::string>& URLComponents)
+	static Response PushShift::PushShiftGet(std::string endpoint, const std::map<std::string, std::string>& URLComponents)
 	{
 		web::uri_builder b(conv::to_string_t("https://api.pushshift.io"));
 		b.set_path(conv::to_string_t(endpoint));
 		b.append_query(conv::to_string_t(Vun::Helpers::GenerateParamData(URLComponents)));
 
-		HttpClient::http_client client(b.to_uri());
+		//HttpClient::http_client client(b.to_uri());
 
-		auto result = client.request(web::http::methods::GET);
-		return result.get();
+		//auto result = client.request(web::http::methods::GET);
+		return rt.Send(, )
 	}
 	
-	Response SearchSubmissions(const std::map<std::string, std::string>& URLComponents)
+	Response PushShift::SearchSubmissions(const std::map<std::string, std::string>& URLComponents)
 	{
 		return PushShiftGet("/reddit/submission/search", URLComponents);
 	}
-	Response SearchComments(const std::map<std::string, std::string>& URLComponents)
+	Response PushShift::SearchComments(const std::map<std::string, std::string>& URLComponents)
 	{
 		return PushShiftGet("/reddit/search/comment", URLComponents);
 	}
--- Vun/PushShift.hpp
+++ Vun/PushShift.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "helpers.hpp"
+#include "Internal/IntervalTracker.hpp"
 #include <cpprest/http_client.h>
 #include <vector>
 #include <string>
@@ -19,9 +20,15 @@ namespace conv = utility::conversions;
 
 typedef web::http::http_response Response;
 
-namespace Vun::PushShift {
-		Response SearchSubmissions(const std::map<std::string, std::string>& URLComponents);
-		Response SearchComments(const std::map<std::string, std::string>& URLComponents);
+namespace Vun {
+	class PushShift {
+	public:
+		static Response SearchSubmissions(const std::map<std::string, std::string>& URLComponents);
+		static Response SearchComments(const std::map<std::string, std::string>& URLComponents);
+	private:
+		static Response PushShiftGet(std::string endpoint, const std::map<std::string, std::string>& URLComponents)
+		static const Vun::Internal::RateTracker rt{ 120 };
+	};
 };
 
 
