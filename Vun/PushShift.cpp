2dd6a172aaf5247d4e4012b1090c9d82cb7e525a
saberman888 <sagittariusz548@gmail.com>
2021-04-07 14:16:57 -0600

    Finished re-implementation of PushShiftGet and implemented GetRateLimiter()

--- Vun/PushShift.cpp
+++ Vun/PushShift.cpp
@@ -3,16 +3,26 @@
 #include <iostream>
 namespace Vun {
 
-	static Response PushShift::PushShiftGet(std::string endpoint, const std::map<std::string, std::string>& URLComponents)
+	Response PushShift::PushShiftGet(std::string endpoint, const std::map<std::string, std::string>& URLComponents)
 	{
-		web::uri_builder b(conv::to_string_t("https://api.pushshift.io"));
+		web::uri_builder b;
+
+		b.set_scheme(utility::conversions::to_string_t("https"));
+		b.set_host(utility::conversions::to_string_t("apiv2.pushshift.io"));
 		b.set_path(conv::to_string_t(endpoint));
 		b.append_query(conv::to_string_t(Vun::Helpers::GenerateParamData(URLComponents)));
 
-		//HttpClient::http_client client(b.to_uri());
+		auto r = web::http::http_request(web::http::methods::GET);
+		r.set_request_uri(b.to_uri());
 
-		//auto result = client.request(web::http::methods::GET);
-		return rt.Send(, )
+		auto rt = GetRateLimiter();
+		return rt.Send(b.to_uri(), r);
+	}
+
+	const Vun::Internal::RateTracker& PushShift::GetRateLimiter()
+	{
+		static const Vun::Internal::RateTracker rt{ 120 };
+		return rt;
 	}
 	
 	Response PushShift::SearchSubmissions(const std::map<std::string, std::string>& URLComponents)
