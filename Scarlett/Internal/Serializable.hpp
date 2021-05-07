387af64ca588b7b6d46b32ca7288055efaa2fff0
Sai <sagittariusz548@gmail.com>
2021-05-07 10:54:34 -0600

    Update Serializable.hpp
    Fix an issue where Serialize would not instantiate with unique_ptr/shared_ptr

--- Scarlett/Internal/Serializable.hpp
+++ Scarlett/Internal/Serializable.hpp
@@ -12,15 +12,11 @@ namespace Scarlett::Internal
 	void Serialize(const std::filesystem::path destination, T& sourceObj, const std::string Tag)
 	{
 		std::ofstream out;
-		in.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+		out.exceptions(std::ofstream::failbit | std::ofstream::badbit);
 		try {
 			out.open(destination);
 			boost::archive::xml_oarchive xoa(out);
-			if constexpr (std::is_same<T, std::shared_ptr<T>>::value || std::is_same<T, std::unique_ptr<T>>::value || std::is_same<T, boost::shared_ptr<T>>::value)
-			{
-				xoa << boost::serialization::make_nvp(Tag.c_str(), *sourceObj.get());
-			}
-			else if constexpr (std::is_pointer<T>::value) {
+			if constexpr (std::is_pointer<T>::value) {
 				xoa << boost::serialization::make_nvp(Tag.c_str(), *sourceObj);
 			}
 			else {
@@ -38,23 +34,27 @@ namespace Scarlett::Internal
 	template<typename T>
 	T DeSerialize(const std::filesystem::path source, const std::string Tag)
 	{
+        
 		std::ifstream in;
 		in.exceptions(std::ofstream::failbit | std::ofstream::badbit);
 		try {
-
-			T temp;
 			in.open(source);
 			boost::archive::xml_iarchive xia(in);
 
 			if constexpr (std::is_pointer<T>::value)
 			{
-				temp = new T;
+                using simpleT = typename std::remove_pointer<T>::type;
+                
+                
+				simpleT* temp = new simpleT;
 				xia >> boost::serialization::make_nvp(Tag.c_str(), *temp);
+                return temp;
 			}
 			else {
+                T temp;
 				xia >> boost::serialization::make_nvp(Tag.c_str(), temp);
+                return temp;
 			}
-			return temp;
 
 		}
 		catch (std::system_error& e) {
