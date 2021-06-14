74115c26c4f32b2548437056184c2d99e58de852
Sai <sagittariusz548@gmail.com>
2021-06-14 09:38:17 -0600

    If guards to check if the xml elements exist and if they're blank. The necessary ones throw exceptions if they're blank

--- Scarlett/tests/scarlett_pch.hpp
+++ Scarlett/tests/scarlett_pch.hpp
@@ -19,16 +19,69 @@ namespace Scarlett::Tests
 		{
 			if (std::filesystem::exists("accessdata.xml"))
 			{
-				tinyxml2::XMLDocument d("accessdata.xml");
+				tinyxml2::XMLDocument d;
+				if(d.LoadFile("accessdata.xml") != tinyxml2::XML_SUCCESS)
+				{
+					throw std::runtime_error("Fatal Error: Failed to open accessdata.xml");
+				}
+
 
 				tinyxml2::XMLElement* root = d.FirstChildElement("accessdata");
 
-				acd.client_key = root->FirstChildElement("client_key")->GetText();
-				acd.client_secret = root->FirstChildElement("client_secret")->GetText();
-				acd.redirect_uri = root->FirstChildElement("redirect_uri")->GetText();
-				acd.useragent = root->FirstChildElement("useragent")->GetText();
-				acd.username = root->FirstChildElement("username")->GetText();
-				acd.password = root->FirstChildElement("password")->GetText();
+				auto client_key = root->FirstChildElement("client_key");
+				if(client_key)
+				{
+					if(client_key->GetText())
+					{
+						acd.client_key = client_key->GetText();
+					} else {
+						throw std::runtime_error("Fatal Error: client_key not found in accessdata.xml");
+					}
+				}
+
+				auto client_secret = root->FirstChildElement("client_secret");
+				if(client_secret)
+				{
+					if(client_secret->GetText())
+					{
+						acd.client_secret->GetText();
+					}
+				}
+
+				auto redirect_uri = root->FirstChildElement("redirect_uri");
+				if(redirect_uri)
+				{
+					if(redirect_uri->GetText() == nullptr)
+					{
+						acd.redirect_uri = "http://localhost:3030";
+					} else {
+						acd.redirect_uri = redirect_uri->GetText();
+					}
+				}
+
+				auto useragent = root->FirstChildElement("useragent");
+				if(useragent)
+				{
+					acd.useragent = useragent->GetText();
+				}
+
+				auto username = root->FirstChildElement("username");
+				if(username)
+				{
+					if(username->GetText())
+					{
+						acd.username = username->GetText();
+					}
+				}
+
+				auto password = root->FirstChildElement("password");
+				if(password)
+				{
+					if(password->GetText())
+					{
+						acd.password = password->GetText();
+					}
+				}
 			}
 			else {
 				throw std::runtime_error("Fatal Error: accessdata.xml is not present!");
