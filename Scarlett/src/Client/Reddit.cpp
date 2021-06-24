8784fda644df11699603cbbaa14ad5fd62e81df8
Sai <sagittariusz548@gmail.com>
2021-06-24 09:35:30 -0600

    Add _XPLATSTR for Windows

--- Scarlett/src/Client/Reddit.cpp
+++ Scarlett/src/Client/Reddit.cpp
@@ -4,7 +4,7 @@ namespace Scarlett::Client
 {
 
   #define _REDDITSCOPE
-  #define DAT(a_, b_) const reddit_scope reddit_scopes::a_(b_);
+  #define DAT(a_, b_) const reddit_scope reddit_scopes::a_(_XPLATSTR(b_));
   #include "Scarlett/Internal/ScarlettConstants.h"
   #undef _REDDITSCOPE
   #undef DAT
