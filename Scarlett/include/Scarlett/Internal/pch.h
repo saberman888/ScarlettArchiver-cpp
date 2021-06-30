e9820e0d1e60cf19f0b6c2e891bbef136c9ce14f
Andrés Vázquez <sagittariusz548@gmail.com>
2021-06-30 05:28:07 -0600

    Precompile headers that don't usually get changed

--- Scarlett/include/Scarlett/Internal/pch.h
+++ Scarlett/include/Scarlett/Internal/pch.h
@@ -8,8 +8,70 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
-#include "exceptions.hpp"
+#include <string>
+#include <exception>
+#include <stdexcept>
+#include <iosfwd>
+#include <utility>
+#include <iomanip>
+#include <filesystem>
+#include <fstream>
+#include <type_traits>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstddef>
+#include <optional>
+#include <regex>
+#include <utility>
+#include <fstream>
+#include <ctime>
+#include <map>
+#include <utility>
+#include <cstring>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/optional.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/serialization/optional.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/export.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/optional.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+#include <cpprest/base_uri.h>
+#include <cpprest/http_msg.h>
+#include <cpprest/http_headers.h>
+#include <cpprest/oauth2.h>
+
+#include <tinyxml2.h>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+#include "Scarlett_def.hpp"
+#include "ScarlettConstants.h"
+#include "Types.hpp"
 #include "Logger.hpp"
-#include "Helpers.hpp"
+#include "Exceptions.hpp"
+#include "Serializable.hpp"
+#include "../Media/Content.hpp"
+#include "../Media/Imgur.hpp"
+#include "../Reddit/BaseTypes/Thing.hpp"
+#include "../Reddit/BaseTypes/Link.hpp"
+#include "../Reddit/BaseTypes/Listing.hpp"
+#include "../Reddit/BaseTypes/TextPost.hpp"
+#include "../Reddit/RedditStatistics.hpp"
+#include "../Reddit/Comment.hpp"
+#include "../Reddit/SelfPost.hpp"
 
 #endif //PCH_H
