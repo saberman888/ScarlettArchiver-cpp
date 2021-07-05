3bbe55b73181ca5bde6ecd6f7e6a7fb6f3f2de10
Sai <sagittariusz548@gmail.com>
2021-07-05 17:45:55 -0600

    Overhaul RateTracker is the same way as the other classes under Scarlett::Client

--- Scarlett/include/Scarlett/Client/RateTracker.hpp
+++ Scarlett/include/Scarlett/Client/RateTracker.hpp
@@ -1,17 +1,6 @@
 #pragma once
 
-#include <chrono>
-#include <optional>
-#include <functional>
-#include <deque>
-#include <tuple>
-#include <cpprest/http_client.h>
-#include <thread>
-#include <algorithm>
-#include <exception>
-#include <mutex>
-#include "../Internal/Helpers.hpp"
-#include "../Internal/Exceptions.hpp"
+#include "../Internal/Types.hpp"
 namespace Scarlett::Client
 {
 	/*
@@ -22,97 +11,29 @@ namespace Scarlett::Client
     class SCDLL RateTracker
 	{
 	public:
-
+		using MinuteRate = int;
+		using Second = std::chrono::seconds;
 		/*
 		* This constructor takes the max amound of acceptable requests per minute, and it divides it by 
 		* 60,000 to get the time difference between each requests in milliseconds
 		*/
-		RateTracker(const int max_rate_minute_limit, std::optional<int> max_time = std::nullopt);
+		RateTracker(const MinuteRate mmr);
+		RateTracker(const MinuteRate mmr, const Second mht);
 		RateTracker(RateTracker& rt);
 
-		/*
-		* This sends the request and makes sure it doesn't exceed any limits. And if the request fails with a code 429,
-		* it will try again but with a longer delay each attempt.
-		*/
-		HttpResponse Send(const URI& srcUri, const HttpRequest& req);
+		HttpResponse Send(const URI& source, const HttpRequest& hr);
 
 		/*
 		* Set the maxumum ammount of tries to retry after a request fails
 		*/
-		inline void SetMaxTries(int n)
-		{
-			MaxTries = n;
-		}
-
-		inline const int GetMaxTries()
-		{
-			return MaxTries;
-		}
-
-		inline void SetMaxTime(std::chrono::seconds time)
-		{
-			this->MaxTime.emplace(time);
-		}
-		
-		// Start tracking time 
-		inline void Track()
-		{
-			if(MaxTime)
-				StartTime.emplace(std::chrono::system_clock::now());
-		}
+		void setMaxTries(const int n);
+		int MaxTries();
+		void setMaxTime(const Second time);
+		Second MaxTime();
+		void Track();
 
 	private:
-		// A temporary storage to keep our intervals
-		std::deque<Millisecond> Cache;
-		Millisecond minimum_time_interval{ 0 };
-		int MaxTries{ 5 };
-		int Tries{ 0 };
-
-		std::optional<std::chrono::seconds> MaxTime{ std::nullopt };
-		std::optional<TimePoint> StartTime{ std::nullopt };
-		std::mutex mlock;
-
-		bool TimeUp();
-
-		inline Millisecond Now() {
-			return std::chrono::duration_cast<Millisecond>(std::chrono::system_clock::now().time_since_epoch());
-		}
-
-		/*
-		* Returns the difference of time between now and the most recent time from Cache in milliseconds, else if 
-		* Cache is empty return 0 milliseconds
-		*/
-		inline Millisecond Delta()
-		{
-			std::lock_guard<std::mutex> guard(mlock);
-			return Cache.empty() ? Millisecond(0) : Millisecond(Now().count() - Cache[0].count());
-		}
-
-		/*
-		* Returns an interval that break the minimum time interval, or is lower than it, and returns it in milliseconds.
-		*/
-		inline Millisecond GetLatestInterval()
-		{
-			std::lock_guard<std::mutex> guard(mlock);
-			UpdateCache();
-			return (minimum_time_interval > Delta()) ? (minimum_time_interval - Delta()) : Millisecond(0);
-		}
-
-		/*
-		* Removes any old data that doesn't break limits
-		*/
-		void UpdateCache();
-
-		inline void UpdateCache(const Millisecond m)
-		{
-			std::lock_guard<std::mutex> guard(mlock);
-			Cache.emplace_back(m);
-		}
-
-		/*
-		* Waits the amount of time if an interval is lower than the minimum, and this goes up the number of requests done represented by n
-		*/
-		void waitifnecessary(int n);
-
+		class _rtImpl;
+		std::unique_ptr<_rtImpl> impl;
 	};
 };
--- Scarlett/src/Client/RateTracker.cpp
+++ Scarlett/src/Client/RateTracker.cpp
@@ -1,93 +1,206 @@
 #include "Scarlett/Client/RateTracker.hpp"
-
+#include "Scarlett/Internal/Exceptions.hpp"
+#include <optional>
+#include <functional>
+#include <deque>
+#include <tuple>
+#include <thread>
+#include <algorithm>
+#include <exception>
+#include <mutex>
+#include <chrono>
+#include <cpprest/http_client.h>
 namespace Scarlett::Client
 {
+
+	class RateTracker::_rtImpl
+	{
+	public:
+		using Millisecond = std::chrono::milliseconds;
+
+		_rtImpl(const MinuteRate mmr, std::optional<Second> max_time = std::nullopt)
+		{
+			minimum_time_interval = Millisecond(60000 / mmr);
+		}
+
+		// A temporary storage to keep our intervals
+		std::deque<Millisecond> Cache;
+		Millisecond minimum_time_interval{ 0 };
+		int MaxTries{ 5 };
+		int Tries{ 0 };
+
+		std::optional<std::chrono::seconds> MaxTime{ std::nullopt };
+		std::optional<TimePoint> StartTime{ std::nullopt };
+		std::mutex mlock;
+
+		// Start tracking a timepoint
+		void Track()
+		{
+			if (MaxTime)
+				StartTime.emplace(std::chrono::system_clock::now());
+		}
+
+		Millisecond Now() {
+			return std::chrono::duration_cast<Millisecond>(std::chrono::system_clock::now().time_since_epoch());
+		}
+
+		bool TimeUp()
+		{
+			std::lock_guard<std::mutex> guard(mlock);
+			if (!StartTime)
+			{
+				if (StartTime.value() >= (StartTime.value() + std::chrono::hours(1)))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/*
+		* Returns the difference of time between now and the most recent time from Cache in milliseconds, else if
+		* Cache is empty return 0 milliseconds
+		*/
+		Millisecond Delta()
+		{
+			std::lock_guard<std::mutex> guard(mlock);
+			return Cache.empty() ? Millisecond(0) : Millisecond(Now().count() - Cache[0].count());
+		}
+
+		/*
+		* Removes any old data that doesn't break limits
+		*/
+		void UpdateCache()
+		{
+			std::lock_guard<std::mutex> guard(mlock);
+			while (Delta() > minimum_time_interval)
+			{
+				Cache.pop_back();
+			}
+		}
+
+		// Add new time to the cache
+		void UpdateCache(const Millisecond m)
+		{
+			std::lock_guard<std::mutex> guard(mlock);
+			Cache.emplace_back(m);
+		}
+
+		/*
+		* Returns an interval that break the minimum time interval, or is lower than it, and returns it in milliseconds.
+		*/
+		Millisecond GetLatestInterval()
+		{
+			std::lock_guard<std::mutex> guard(mlock);
+			UpdateCache();
+			return (minimum_time_interval > Delta()) ? (minimum_time_interval - Delta()) : Millisecond(0);
+		}
+
+		/*
+		* Waits the amount of time if an interval is lower than the minimum, and this goes up the number of requests done represented by n
+		*/
+		void waitifnecessary(int n)
+		{
+			// Get an interval if there's any
+			Millisecond interval = GetLatestInterval();
+
+			// Multiply the number of tries by 200ms and make sure it's under 1 hour
+			Millisecond waitTimes = Millisecond(200 * n);
+			waitTimes = waitTimes.count() < 3600 * 1000 ? waitTimes : Millisecond(3600 * 100);
+
+			std::this_thread::sleep_for(interval);
+			std::this_thread::sleep_for(waitTimes);
+
+		}
+
+		/*
+		* This sends the request and makes sure it doesn't exceed any limits. And if the request fails with a code 429,
+		* it will try again but with a longer delay each attempt.
+		*/
+		HttpResponse Send(const URI& srcUri, const HttpRequest& req)
+		{
+			web::http::client::http_client cl{ srcUri.authority().to_string() };
+			HttpResponse hr;
+
+			if (TimeUp())
+			{
+				scarlettOOTThrow("Error: Token's time has expired");
+			}
+
+			if (Tries >= MaxTries)
+			{
+				scarlettThrow("Error: Max number of requests reached");
+			}
+
+			bool complete = false;
+			int tries = 0;
+			while (!complete && tries < MaxTries)
+			{
+				// Depending on how many retries and how long the last attempt was wait if neccessary
+				waitifnecessary(tries);
+
+				// Next, initiate a request, time it, and push it into cache
+				auto beginTime = Now();
+				hr = cl.request(req).get();
+				UpdateCache(Now() - beginTime);
+
+				tries++;
+
+				if (hr.status_code() != 429 || hr.status_code() == 200)
+				{
+					complete = true;
+					break;
+				}
+			}
+
+			return hr;
+		}
+	};
+
+
+	RateTracker::RateTracker(const MinuteRate mmr)
+	{
+		impl = std::make_unique<_rtImpl>(mmr);
+	}
+
+	RateTracker::RateTracker(const MinuteRate mmr, const Second mht)
+	{
+		impl = std::make_unique<_rtImpl>(mmr, mht);
+	}
+
 	RateTracker::RateTracker(RateTracker& rt)
 	{
-		rt.Cache = Cache;
-		rt.minimum_time_interval = minimum_time_interval;
-		rt.MaxTries = MaxTries;
-		rt.Tries = Tries;
-		rt.MaxTime = MaxTime;
-		rt.StartTime = StartTime;
+		impl.reset(rt.impl.get());
 	}
-	bool RateTracker::TimeUp()
+
+	void RateTracker::setMaxTries(int n)
 	{
-		std::lock_guard<std::mutex> guard(mlock);
-		if(!StartTime)
-		{
-			if(StartTime.value() >= (StartTime.value() + std::chrono::hours(1)))
-			{
-				return true;
-			}
-		}
-		return false;
+		impl->MaxTries = n;
 	}
-	void RateTracker::UpdateCache()
+
+	int RateTracker::MaxTries()
 	{
-		std::lock_guard<std::mutex> guard(mlock);
-		while (Delta() > minimum_time_interval)
-		{
-			Cache.pop_back();
-		}
+		return impl->MaxTries;
 	}
 
-	void RateTracker::waitifnecessary(int n)
+	void RateTracker::setMaxTime(const Second time)
 	{
-		// Get an interval if there's any
-        Millisecond interval = GetLatestInterval();
-
-		// Multiply the number of tries by 200ms and make sure it's under 1 hour
-		Millisecond waitTimes = Millisecond(200 * n);
-		waitTimes = waitTimes.count() < 3600 * 1000 ? waitTimes : Millisecond(3600 * 100);
-
-		std::this_thread::sleep_for(interval);
-		std::this_thread::sleep_for(waitTimes);
-
+		impl->MaxTime.emplace(time);
 	}
 
-	RateTracker::RateTracker(int max_rate_minute_limit, std::optional<int> max_time)
+	void RateTracker::Track()
 	{
-
-		this->minimum_time_interval = Millisecond(60000 / max_rate_minute_limit);
+		impl->Track();
 	}
 
-	HttpResponse RateTracker::Send(const URI& srcUri, const HttpRequest& req)
+	std::chrono::seconds RateTracker::MaxTime()
 	{
-		web::http::client::http_client cl{ srcUri.authority().to_string() };
-		HttpResponse hr;
-
-		if(TimeUp())
-		{
-			scarlettOOTThrow("Error: Token's time has expired");
-		}
-
-		if(Tries >= MaxTries)
-		{
-			scarlettThrow("Error: Max number of requests reached");
-		}
-
-		bool complete = false;
-		int tries = 0;
-		while (!complete && tries < MaxTries)
-		{
-            		// Depending on how many retries and how long the last attempt was wait if neccessary
-			waitifnecessary(tries);
-
-            		// Next, initiate a request, time it, and push it into cache
-			auto beginTime = Now();
-			hr = cl.request(req).get();
-			UpdateCache(Now() - beginTime);
-
-			tries++;
-
-			if (hr.status_code() != 429 || hr.status_code() == 200)
-			{
-				complete = true;
-				break;
-			}
-		}
-
-		return hr;
+		return impl->MaxTime.value();
 	}
+
+	HttpResponse RateTracker::Send(const URI& source, const HttpRequest& hr)
+	{
+		return impl->Send(source, hr);
+	}
+
 };
