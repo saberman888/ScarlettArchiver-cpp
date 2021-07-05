48cd2d7ac8c19f380b488c323325895571ce7e0b
Sai <sagittariusz548@gmail.com>
2021-07-05 16:50:34 -0600

    Overhaul RedditClient in the same way I did with OAuth2 and redesign it with PImple in mind.
    Remove Me()
    Temporarily comment out RedditGet until I can mplement a good substitute for Send
    
    Remove all references to RateTracker
    Fix any mistakes and mark RedditClient for DLL export
    Explicitly instantiate all specializations for RedditClient

--- Scarlett/include/Scarlett/Client/Reddit.hpp
+++ Scarlett/include/Scarlett/Client/Reddit.hpp
@@ -1,15 +1,10 @@
 #pragma once
 
 #include <string>
-#include <algorithm>
-#include <random>
 #include <memory>
-#include <cpprest/http_client.h>
-#include <cpprest/json.h>
 #include <string_view>
-#include "OAuth2.hpp"
-#include <fstream>
 #include "../Internal/Types.hpp"
+#include "AccessData.hpp"
 
 namespace Scarlett::Client
 {
@@ -24,90 +19,27 @@ namespace Scarlett::Client
 #undef DAT
 	};
 
+	// TODO: Implement methods take use the API
 
-	class PasswordGrant, AuthorizationGrant;
+	class PasswordGrant;
+	class AuthorizationGrant;
 
 	template<typename T>
-	class RedditClient
+	class SCDLL RedditClient
 	{
 	public:
-		RedditClient(struct AccessData& acd)
-		{
+		RedditClient(const struct AccessData& acd);
+		RedditClient(RedditClient& other);
+		RedditClient(RedditClient&& other);
+		RedditClient(const String client_key, const String client_secret, const String redirect_uri, const String useragent);
+		RedditClient(const String username, const String password, const String client_key, const String client_secret, const String redirect_uri, const String useragent);
 
-			oauth2handle = std::make_shared< OAuth2Authorization<T> >(
-				acd.username, 
-				acd.password, 
-				acd.client_key, 
-				acd.client_secret, 
-				acd.redirect_uri, 
-				acd.useragent);
+		bool AuthorizeWithReddit();
 
-			oauth2handle->SetMaxTime(
-				std::chrono::seconds(3600)
-			);
-		}
-
-		void Me()
-		{
-			auto v = RedditGet("/api/v1/me", StringMap{});
-			auto r = v.extract_string().get();
-		}
-
-		void AuthorizeWithReddit()
-		{
-			try {
-				GenerateScopes();
-				oauth2handle->GetToken();
-				oauth2handle->Track();
-			}
-			catch (std::exception& e) {
-				scarlettNestedThrow(e.what());
-			}
-		}
-
-
-
-		RedditClient<T>& operator<<(const reddit_scope& scope)
-		{
-			scopes.push_back(scope);
-			return *this;
-		}
-
-
+		RedditClient& operator<<(const reddit_scope& scope);
 	private:
-		std::vector<reddit_scope> scopes;
-		void GenerateScopes()
-		{
-			String scope_string;
-			if(scopes.size() != 0)
-			{
-
-				for(auto& scope : scopes)
-				{
-					scope_string += scope;
-					scope_string += " "_u;
-				}
-			} else {
-				scope_string = "*"_u;
-			}
-			oauth2handle->setScope(scope_string);
-		}
-
-		auto RedditGet(const std::string& endpoint, const std::optional<StringMap> query_parameters)
-		{
-			web::uri_builder ub;
-			ub.set_scheme("https"_u);
-			ub.set_host("api.reddit.com"_u);
-			ub.set_path(toScarlettString(endpoint));
-			if(query_parameters){
-				ub.append_query(toScarlettString(GenerateParamData(query_parameters.value())));
-			}
-			auto r = HttpRequest(web::http::methods::GET);
-			r.set_request_uri(ub.to_uri());
-			return oauth2handle->Send(ub.to_uri(), r);
-		}
-
-		std::shared_ptr< OAuth2Authorization > oauth2handle;
+		class _rcImpl;
+		std::unique_ptr<_rcImpl> impl;
 	};
 	using AuthClient = RedditClient<AuthorizationGrant>;
 	using SimpleClient = RedditClient<PasswordGrant>;
--- Scarlett/src/Client/Reddit.cpp
+++ Scarlett/src/Client/Reddit.cpp
@@ -1,11 +1,122 @@
 #include "Scarlett/Client/Reddit.hpp"
+#include "Scarlett/Client/OAuth2.hpp"
+
 
 namespace Scarlett::Client
 {
 
-  #define _SCARLETT_REDDIT_SCOPE
-  #define DAT(a_, b_) const reddit_scope reddit_scopes::a_(SCARLETT_WIDEN(b_));
-  #include "Scarlett/Internal/ScarlettConstants.h"
-  #undef _SCARLETT_REDDIT_SCOPE
-  #undef DAT
+#define _SCARLETT_REDDIT_SCOPE
+#define DAT(a_, b_) const reddit_scope reddit_scopes::a_(SCARLETT_WIDEN(b_));
+#include "Scarlett/Internal/ScarlettConstants.h"
+#undef _SCARLETT_REDDIT_SCOPE
+#undef DAT
+
+	template<typename T>
+	RedditClient<T>::RedditClient(const struct AccessData& acd)
+	{
+		impl = std::make_unique<_rcImpl>(acd);
+	}
+
+	template<typename T>
+	RedditClient<T>::RedditClient(const String client_key, const String client_secret, const String redirect_uri, const String useragent)
+	{
+		impl = std::make_unique<_rcImpl>(client_key, client_secret, redirect_uri, useragent);
+	}
+
+	template<typename T>
+	RedditClient<T>::RedditClient(const String username, const String password, const String client_key, const String client_secret, const String redirect_uri, const String useragent)
+	{
+		impl = std::make_unique<_rcImpl>(client_key, client_secret, redirect_uri, useragent);
+		impl->oauth2handle->setUserCredentials(username, password);
+	}
+
+	template<typename T>
+	RedditClient<T>::RedditClient(RedditClient<T>& other)
+	{
+		impl->reset(other.impl.get());
+	}
+
+	template<typename T>
+	bool RedditClient<T>::AuthorizeWithReddit()
+	{
+		return impl->AuthorizeWithReddit();
+	}
+
+
+
+	template<typename T>
+	class RedditClient<T>::_rcImpl
+	{
+	public:
+		std::shared_ptr<OAuth2Authorization> oauth2handle;
+		std::vector<reddit_scope> scopes;
+
+
+		_rcImpl(const struct AccessData& acd)
+		{
+			oauth2handle = std::make_shared<OAuth2Authorization>(acd);
+		}
+
+		_rcImpl(const String client_key, const String client_secret, const String redirect_uri, const String useragent)
+		{
+			oauth2handle = std::make_shared<OAuth2Authorization>(client_key, client_secret, redirect_uri, useragent);
+		}
+
+		void AuthorizeWithReddit()
+		{
+			if constexpr (std::is_same_v<T, PasswordGrant>)
+			{
+				return oauth2handle->RequestTokenFromPassword();
+			}
+			else {
+				return oauth2handle->RequestTokenFromCode();
+			}
+			return false;
+		}
+
+
+		RedditClient<T>& operator<<(const reddit_scope& scope)
+		{
+			scopes.push_back(scope);
+			return *this;
+		}
+
+		void GenerateScopes()
+		{
+			String scope_string;
+			if (scopes.size() != 0)
+			{
+
+				for (auto& scope : scopes)
+				{
+					scope_string += scope;
+					scope_string += " "_u;
+				}
+			}
+			else {
+				scope_string = "*"_u;
+			}
+			oauth2handle->setScope(scope_string);
+		}
+		// TODO: Implement GET and POST functions that take the oauth2 tokens
+		/*auto RedditGet(const std::string& endpoint, const std::optional<StringMap> query_parameters)
+		{
+			web::uri_builder ub;
+			ub.set_scheme("https"_u);
+			ub.set_host("api.reddit.com"_u);
+			ub.set_path(toScarlettString(endpoint));
+			if (query_parameters) {
+				ub.append_query(toScarlettString(GenerateParamData(query_parameters.value())));
+			}
+			auto r = HttpRequest(web::http::methods::GET);
+			r.set_request_uri(ub.to_uri());
+			return oauth2handle->Send(ub.to_uri(), r);
+		}*/
+
+
+	};
+
+	template class RedditClient<PasswordGrant>;
+	template class RedditClient<AuthorizationGrant>;
+
 }
