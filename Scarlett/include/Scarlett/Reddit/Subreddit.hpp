47bc278399cdd6f7afb7ae372d4493454695750f
Sai <sagittariusz548@gmail.com>
2021-07-07 16:32:56 -0600

    Move inline functions into the cpp file and explicitly instantiate template functions

--- Scarlett/include/Scarlett/Reddit/Subreddit.hpp
+++ Scarlett/include/Scarlett/Reddit/Subreddit.hpp
@@ -1,20 +1,8 @@
 #pragma once
 
-#include "../Client/PushShift.hpp"
-#include "Video.hpp"
-#include "SelfPost.hpp"
-#include "Galleries.hpp"
-#include "SubredditMetadata.hpp"
-#include "../Media/Content.hpp"
-#include "../Internal/Serializable.hpp"
+
 #include "BaseTypes/Listing.hpp"
-#include <map>
-#include <utility>
-#include <cstring>
-#include <chrono>
-#include <thread>
-#include <algorithm>
-#include <fstream>
+#include "SubredditMetadata.hpp"
 
 namespace Scarlett::Reddit {
     class SCDLL Subreddit : public Listing<Thing>, protected Logger
@@ -31,12 +19,7 @@ namespace Scarlett::Reddit {
 		*	HasNext checks if there are any potential posts we could fetch.
 		*	It does this by getting the differrence of the end date and current position in time
 		*/
-		inline bool HasNext() override
-		{
-			return !(
-				(sub->End() - sub->Position()) <= 0
-				);
-		}
+		bool HasNext() override;
 
 		void Save(const std::filesystem::path location, bool clear = true);
 		void Load(const std::filesystem::path location);
@@ -45,76 +28,9 @@ namespace Scarlett::Reddit {
 		std::unique_ptr<SubredditMetadata> sub;
 
 		template<class T>
-		void WriteMedia(const boost::shared_ptr<T> post, const std::filesystem::path location)
-		{
-			using namespace std::filesystem;
-
-			const auto mediaPath = location / "media";
-			create_directories(mediaPath);
-			
-
-			if constexpr (std::is_same<Gallery, T>::value)
-			{
-				const std::filesystem::path galPath = mediaPath / toString(post->getId());
-				std::filesystem::create_directories(galPath);
-				
-				std::vector<Media::Content> images = post->GetImages();
-
-				for (auto i = 0; i < images.size()-1; i++)
-				{
-					Media::Content& c = images[i];
-					if (c.FetchContent() != 200)
-					{
-						scarlettThrow("Failed to download Gallery image: " + toString(c.GetURLString()));
-					}
-					else {
-						auto filename = galPath / (toString(post->getId()) + "_" + std::to_string(i) + "." + toString(c.Extension()));
-						std::ofstream out(filename.string(), std::ios::binary | std::ios::out);
-
-						auto data_vec = c.GetContent();
-						for(auto& data : data_vec)
-						{
-							out << data;
-						}
-					}
-				}
-			}
-			else if constexpr (std::is_same<Link, T>::value)
-			{
-				auto sc = post->getURL().FetchContent();
-				if (sc != 200)
-				{
-					scarlettThrow("Failed to download content from Link, " + toString(post->getURL().GetURLString()));
-				}
-
-				if (post->getURL().ContentType() == "image"_u) {
-					auto filename = mediaPath / path(toString(post->getId() + "."_u + post->getURL().Extension()));
-					std::ofstream out(filename.string(), std::ios::binary | std::ios::out);
-
-					auto data_vec = post->getURL().GetContent();
-					for(auto& data : data_vec)
-					{
-						out << data;
-					}
-				}
-			}
-		}
-
+		void WriteMedia(const boost::shared_ptr<T> post, const std::filesystem::path location);
 
 		template<class T>
-		void WritePost(boost::shared_ptr<T> post, const String tag, const std::filesystem::path location)
-		{
-			using namespace std::filesystem;
-
-			auto time = post->getCreatedUTCTime();
-			auto tempTime = *std::gmtime(&time);
-			auto destination = location /  path(formatTime(tempTime, "%Y")) / path(formatTime(tempTime, "%m")) / formatTime(tempTime, "%d");
-			String filename = post->getId() + ".xml"_u;
-
-			std::filesystem::create_directories(destination);
-			//scout << "Writing "_u << post->getId() << " to "_u << toScarlettString(destination.string()) << "\n"_u;
-			Internal::Serialize<boost::shared_ptr<T>>(destination / toString(filename), post, toString(tag));
-
-		}
+		void WritePost(boost::shared_ptr<T> post, const String tag, const std::filesystem::path location);
 	};
 }
--- Scarlett/src/Reddit/Subreddit.cpp
+++ Scarlett/src/Reddit/Subreddit.cpp
@@ -1,5 +1,19 @@
 #include "Scarlett/Reddit/Subreddit.hpp"
+#include "Scarlett/Client/PushShift.hpp"
+#include "Scarlett/Reddit/Video.hpp"
+#include "Scarlett/Reddit/SelfPost.hpp"
+#include "Scarlett/Reddit/Galleries.hpp"
+#include "Scarlett/Media/Content.hpp"
+#include "Scarlett/Internal/Serializable.hpp"
+#include "Scarlett/Internal/Exceptions.hpp"
 #include <iostream>
+#include <map>
+#include <utility>
+#include <cstring>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <fstream>
 
 namespace Scarlett::Reddit {
 
@@ -7,6 +21,13 @@ namespace Scarlett::Reddit {
 	{
 		sub = std::make_unique<SubredditMetadata>(Subreddit, Start, End);
 		log->info("{} has been instantiated.", toString(sub->Subreddit()));
+	}
+
+	bool Subreddit::HasNext()
+	{
+		return !(
+			(sub->End() - sub->Position()) <= 0
+			);
 	}
 
 	void Subreddit::Next()
@@ -97,4 +118,85 @@ namespace Scarlett::Reddit {
 		}
 	}
 
+	template<class T>
+	void Subreddit::WriteMedia(const boost::shared_ptr<T> post, const std::filesystem::path location)
+	{
+		using namespace std::filesystem;
+
+		const auto mediaPath = location / "media";
+		create_directories(mediaPath);
+
+
+		if constexpr (std::is_same<Gallery, T>::value)
+		{
+			const std::filesystem::path galPath = mediaPath / toString(post->getId());
+			std::filesystem::create_directories(galPath);
+
+			std::vector<Media::Content> images = post->GetImages();
+
+			for (auto i = 0; i < images.size() - 1; i++)
+			{
+				Media::Content& c = images[i];
+				if (c.FetchContent() != 200)
+				{
+					scarlettThrow("Failed to download Gallery image: " + toString(c.GetURLString()));
+				}
+				else {
+					auto filename = galPath / (toString(post->getId()) + "_" + std::to_string(i) + "." + toString(c.Extension()));
+					std::ofstream out(filename.string(), std::ios::binary | std::ios::out);
+
+					auto data_vec = c.GetContent();
+					for (auto& data : data_vec)
+					{
+						out << data;
+					}
+				}
+			}
+		}
+		else if constexpr (std::is_same<Link, T>::value)
+		{
+			auto sc = post->getURL().FetchContent();
+			if (sc != 200)
+			{
+				scarlettThrow("Failed to download content from Link, " + toString(post->getURL().GetURLString()));
+			}
+
+			if (post->getURL().ContentType() == "image"_u) {
+				auto filename = mediaPath / path(toString(post->getId() + "."_u + post->getURL().Extension()));
+				std::ofstream out(filename.string(), std::ios::binary | std::ios::out);
+
+				auto data_vec = post->getURL().GetContent();
+				for (auto& data : data_vec)
+				{
+					out << data;
+				}
+			}
+		}
+	}
+
+	template<class T>
+	void Subreddit::WritePost(boost::shared_ptr<T> post, const String tag, const std::filesystem::path location)
+	{
+		using namespace std::filesystem;
+
+		auto time = post->getCreatedUTCTime();
+		auto tempTime = *std::gmtime(&time);
+		auto destination = location / path(formatTime(tempTime, "%Y")) / path(formatTime(tempTime, "%m")) / formatTime(tempTime, "%d");
+		String filename = post->getId() + ".xml"_u;
+
+		std::filesystem::create_directories(destination);
+		//scout << "Writing "_u << post->getId() << " to "_u << toScarlettString(destination.string()) << "\n"_u;
+		Internal::Serialize<boost::shared_ptr<T>>(destination / toString(filename), post, toString(tag));
+
+	}
+
+	template void Subreddit::WriteMedia(const boost::shared_ptr<Link> post, const std::filesystem::path location);
+	template void Subreddit::WriteMedia(const boost::shared_ptr<Gallery> post, const std::filesystem::path location);
+
+	template void Subreddit::WritePost(boost::shared_ptr<Link> post, const String tag, const std::filesystem::path location);
+	template void Subreddit::WritePost(boost::shared_ptr<Gallery> post, const String tag, const std::filesystem::path location);
+	template void Subreddit::WritePost(boost::shared_ptr<Comment> post, const String tag, const std::filesystem::path location);
+	template void Subreddit::WritePost(boost::shared_ptr<SelfPost> post, const String tag, const std::filesystem::path location);
+	template void Subreddit::WritePost(boost::shared_ptr<RedditStatistics> post, const String tag, const std::filesystem::path location);
+	template void Subreddit::WritePost(boost::shared_ptr<Video> post, const String tag, const std::filesystem::path location);
 }
